3981ac4224ab1726cd83d14acb12b353c1fb395e
d-logic <149225137+rdembski@users.noreply.github.com>
2024-07-23 18:10:50 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,4 @@ apt-get update
 apt-get install -y tesseract-ocr
 pip install -r requirements.txt
 pip install streamlit
-#!/bin/bash
-# Example setup commands
-echo "Setting up environment..."
-# Add your setup commands here
+
