41e3880febaa990eb01cf564d560db4635b106b2
d-logic <149225137+rdembski@users.noreply.github.com>
2024-07-23 10:18:02 +0200

    Create Tabs.svelte

---
+++ Tabs.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { onMount } from 'svelte';
+    let isTabVisible = false;
+
+    onMount(() => {
+        isTabVisible = true;
+    });
+
+    function selectTab() {
+        if (isTabVisible) {
+            // Kod do wyboru zakładki
+            console.log("Tab selected");
+        }
+    }
+</script>
+
+<button on:click={selectTab}>Wybierz zakładkę</button>
