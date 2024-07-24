f1899959f52629425d3e3d09fcdcdebae8b79ee2
d-logic <149225137+rdembski@users.noreply.github.com>
2024-07-24 17:52:50 +0200

    Delete background.js

--- background.js
+++
@@ -1,8 +0,0 @@
-// background.js
-
-browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    return new Promise((resolve, reject) => {
-        // Twoje asynchroniczne operacje
-        resolve();
-    });
-});
